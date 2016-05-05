88f366e53c091b4e64173d29a9fb913ef470e1a2
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2016-05-05 10:29:36 -0700

    Move configuration of bot to its own file

--- hushmachine.rb
+++ hushmachine.rb
@@ -1,4 +1,5 @@
 $:.unshift(Dir.pwd)
+Process.setproctitle('hushmachine')
 
 # Look at https://github.com/dominikh/Mathetes/blob/cinch_rewrite/bot.rb for some ideas
 
@@ -9,46 +10,13 @@ require 'cinch/plugins/basic_ctcp'
 require 'yaml'
 
 %w{reconnect convenience bored drawalong help quotes tags fzoo subchecker tell hug shush arttip linkreader flirt commandlogger poll}.each do |plugin|
-  require "plugins/#{plugin}"
+  require_relative "plugins/#{plugin}"
 end
 
 bot = Cinch::Bot.new do
   configure do |c|
-    settings = YAML.load_file('config.yaml') || {:plugins => {}}
-
-    # Eventually all (or at least most) settings will be in the file rather
-    # than being specified here
-    c.server = "irc.freenode.net"
-    c.channels = ["#reddit-mlpds", "#reddit-mlpds-spoilers", "#reddit-mlpds-bots"]
-    c.nicks = ["hushmachine", "FineLineFan", "MLPDSmachine"]
-    c.realname = "mk2"
-    c.user = "mk2"
-    c.plugins.plugins = [
-                          Cinch::Plugins::BasicCTCP,
-                          Cinch::Plugins::Identify,
-
-                          Arttip,
-                          Bored,
-                          Convenience,
-                          CommandLogger,
-                          Drawalong,
-                          Fzoo,
-                          Flirt,
-                          Help,
-                          Hug,
-                          LinkReader,
-                          Quotes,
-                          Reconnect,
-                          Shush,
-                          SubChecker,
-                          Tags,
-                          Tell,
-                          Poll
-                        ]
-
-    settings[:plugins].each do |plugin, options|
-      c.plugins.options[plugin] = options
-    end
+    c.load YAML.load_file 'config.yaml'
+    #settings = YAML.load_file('config.yaml') || {:plugins => {}}
   end
 end
 
@@ -59,16 +27,17 @@ end
 bot.loggers.level = :log
 #bot.loggers.level = :debug
 
-while (@cmd = gets) != "/quit\n"
-  break if @cmd == "/quit"
-  if @cmd.start_with? "/me"
-    bot.Channel("#reddit-mlpds").action(@cmd[4..-1])
+while (@cmd = gets) != '/quit\n'
+  if @cmd.start_with? '/quit'
+    break
+  elsif @cmd.start_with? '/me'
+    bot.Channel('#reddit-mlpds').action(@cmd[4..-1])
   else
-    bot.Channel("#reddit-mlpds").send(@cmd)
+    bot.Channel('#reddit-mlpds').send(@cmd)
   end
 end
 
-bot.quit("Killed by console")
+bot.quit('Killed by console')
 
 sleep 2
 
